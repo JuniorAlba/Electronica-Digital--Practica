e6a6b7db903a0d29188c5b974d6020082cc556dd
JuniorAlba <juninhoalba@gmail.com>
2025-05-10 09:37:36 -0300

    Ejercicio 2

--- Guia2/Ejercicio 2/eje2.v
+++ Guia2/Ejercicio 2/eje2.v
@@ -1,12 +1,13 @@
-module eje1 (
+module eje2 (
     input wire clk,
     input wire reset,
     input wire A,  // Cambiado a entrada
     input wire B,  // Cambiado a entrada
-    output wire and1     // Salida correcta
+    input wire C,  // Cambiado a entrada
+    output wire nor1    // Salida correcta
 );
 
     // Operación lógica para la salida
-    assign and1 = A & B;
+    assign nor1 = ~(A | B | C);
 
 endmodule
--- Guia2/Ejercicio 2/eje2_tb.v
+++ Guia2/Ejercicio 2/eje2_tb.v
@@ -1,7 +1,7 @@
 `default_nettype none
 `timescale 100 ns / 10 ns
 
-module eje1_tb();
+module eje2_tb();
 
 //-- Parámetro para definir cuánto dura la simulación
 parameter DURATION = 100; // Cambiá esto según lo que necesites
@@ -9,40 +9,46 @@ parameter DURATION = 100; // Cambiá esto según lo que necesites
 //-- Señales de entrada del módulo
 reg clk = 0;
 reg reset;
-reg salida1;  // Cambiado a reg para poder asignar valores
-reg salida2;  // Cambiado a reg para poder asignar valores
+reg val1;  // Cambiado a reg para poder asignar valores
+reg val2;  // Cambiado a reg para poder asignar valores
+reg val3;  // Cambiado a reg para poder asignar valores
 
 //-- Señales de salida del módulo
-wire and1;  // Salida del módulo bajo prueba
+wire nor1;  // Salida del módulo bajo prueba
 
-reg [8:0] j ;
-reg [8:0] i ;
+reg [18:0] j ;
+reg [18:0] i ;
+reg [18:0] z ;
+
 //-- Instanciación del módulo bajo prueba
-eje1 UUT (
+eje2 UUT (
     .clk(clk),
     .reset(reset),
-    .A(salida1),
-    .B(salida2),
-    .and1(and1)
+    .A(val1),
+    .B(val2),
+    .C(val3),
+    .nor1(nor1)
 );
 integer n;
 
 //-- Bloque inicial: estímulos y configuración
 initial begin
   // Archivo para almacenar la traza
-  $dumpfile("eje1_tb.vcd");
-  $dumpvars(0, eje1_tb);
+  $dumpfile("eje2_tb.vcd");
+  $dumpvars(0, eje2_tb);
   reset = 1;
   #1;
   reset = 0;
 
   // Generar estímulos para las entradas
-  i = 9'b001000100;
-  j = 9'b011101110;
+  i = 19'b0111111000011110000;
+  j = 19'b0000111100001111000;
+  z = 19'b0010010001000100010;
 
-  for (n = 0; n < 9; n = n + 1) begin
-    salida1 = j[n];
-    salida2 = i[n];
+  for (n = 0; n < 19 ; n = n + 1) begin
+    val1 = i[18-n];
+    val2 = j[18-n];
+    val3 = z[18-n];
     #1; // Esperar un ciclo de reloj
   end
 
